96c2e8ae0effa372f033c98a0e7eea973291ff66
Mamadiyor <85351016+Annaqulov@users.noreply.github.com>
2021-07-23 15:35:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var mamadiyoor = prompt('ismingizni kiriting');
+console.log('Mening ' + 'ismim ' + mamadiyoor);
+
+var mamadiyorr = +prompt( mamadiyoor + ' yoshingiz nechchida');
+console.log('Men ' + mamadiyorr + ' yoshdaman');
+
+var mamadiyor = +prompt(mamadiyoor + ' 6 ' + '+ ' + '3 ' + '=' + '?' + ' javobini' + ' toping' );
+console.log('Misol ' + '1: ' + '6 ' + '+' + ' 3 ' + '= ' + mamadiyor + ' sizning ' + 'javob ' + mamadiyor);
+
+var mamadiyor = +prompt('20 ' + '- ' + '5 ' + '=' + '?' + ' javobini' + ' toping' );
+console.log('Misol ' + '2: ' + '20 ' + '-' + ' 5 ' + '= ' + mamadiyor + ' sizning ' + 'javob ' + mamadiyor);
+
+var mamadiyor = +prompt('15 ' + '* ' + '2 ' + '=' + '?' + ' javobini' + ' toping' );
+console.log('Misol ' + '3: ' + '15 ' + '*' + ' 2 ' + '= ' + mamadiyor + ' sizning ' + 'javob ' + mamadiyor);
+
+var mamadiyor = +prompt('8 ' + '/ ' + '4 ' + '=' + '?' + ' javobini' + ' toping' );
+console.log('Misol ' + '4: ' + '8 ' + '/' + ' 4 ' + '= ' + mamadiyor + ' sizning ' + 'javob ' + mamadiyor);
+
+var mamadiyor = +prompt('10 ' + '% ' + '3 ' + '=' + '?' + ' javobini' + ' toping' );
+console.log('Misol ' + '5: ' + '10 ' + '%' + ' 3 ' + '= ' + mamadiyor + ' sizning ' + 'javob ' + mamadiyor);
+
+var mamadiyorr = +prompt( mamadiyoor + ' siz' + ' barcha' + ' savollarimga' + ' javob' + ' berdingiz' + ' F12' + ' tugmani' + ' bosib' + ' console' + ' bulimiga' + ' kirib' + ' kurishingiz' + ' mumkin')
